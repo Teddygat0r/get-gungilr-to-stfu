502140a0e1a6c5adb941e16710a88c6914915387
Teddygat0r <zhy0224@gmail.com>
2023-11-30 21:25:03 -0800

    v1

---
+++ main.py
@@ -0,0 +1,28 @@
+import pyaudio
+import numpy as np
+
+CHUNK = 1024
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=pyaudio.paInt16,
+                channels=1,
+                rate=44100,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("Listening...")
+try:
+    while True:
+        data = stream.read(CHUNK)
+        
+        audio_data = np.frombuffer(data, dtype=np.int16)
+        
+        print(f"Max: {np.max(audio_data)}, Min: {np.min(audio_data)}")
+        
+# Get gun to stfu by turning pc off
+# Get gun to stfu by playing funny audio 
+# Get gun to stfu by taking his discord auth token
+
+except KeyboardInterrupt:
+    pass
